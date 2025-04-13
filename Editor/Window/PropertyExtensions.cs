dbdd7aeab6634ee3e829b9d812e53a89276448e0
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-04-13 16:25:07 +0300

    [project] removed unused PropertyExtensions

--- Editor/Window/PropertyExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CustomMenu.Editor.Window
-{
-    internal static class PropertyExtensions
-    {
-        internal static string ConvertToBackingField(this string propertyName)
-            => $"<{propertyName}>k__BackingField";
-    }
-}
